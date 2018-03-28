aa706a83f4d1b73253e20e2265315d3f9ab85d36
Philippe Melillo <melillophilippe@gmail.com>
2018-03-27 21:12:00 -0400

    TASK-5: added pagination for todo list

--- src/controllers.php
+++ src/controllers.php
@@ -54,11 +54,49 @@ $app->get('/todo/{id}', function ($id) use ($app) {
             'todo' => $todo,
         ]);
     } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+
+        if(isset($_GET['p']) && is_numeric($_GET['p']) ){
+            $page_number = intval( $_GET['p'] ) ;
+        } else {
+            $page_number = 1;
+        }
+              
+        $page_size = 2;
+        $offset = ($page_number-1) * $page_size;
+
+        $sql_total =  "SELECT count(*) as total FROM todos WHERE user_id = '${user['id']}'";
+        $total_todos = intval($app['db']->fetchAssoc($sql_total)['total']);
+    
+        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}' ORDER BY id LIMIT $offset, $page_size ";
         $todos = $app['db']->fetchAll($sql);
 
+        $previous = null;
+        if(($page_number - 1)>0){
+            $previous = '?p=' . ($page_number - 1);
+        }
+
+        $next = null;
+        if($total_todos > ($page_number * $page_size)){
+            $next = '?p=' . ($page_number + 1);
+        }
+
+        $number_of_pages = ceil($total_todos / $page_size);
+
+        $pages = array();
+        for($i = 1; $i <= $number_of_pages; $i++ ){
+            $link_text =$i;
+            $class = "";
+            if($i == $page_number){
+                $class = "active";
+            }
+            array_push($pages, array('link_text'=> $link_text , 'link' => '?p=' . $i, 'class' => $class));
+        }
+     
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
+            'next' => $next,
+            'previous' => $previous,
+            'pages' => $pages
         ]);
     }
 })
